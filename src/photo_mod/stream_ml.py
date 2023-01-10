69680f3717c874862704b9e5a8f3246a240aa8d7
heatheranndye <32517963+heatheranndye@users.noreply.github.com>
2023-01-10 09:52:57 -0600

    Change file references

--- src/photo_mod/stream_ml.py
+++ src/photo_mod/stream_ml.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import pathlib
+from PIL import Image
 import json
 import requests
 
+DATAPATH = pathlib.Path(__file__).parent / "data"
 
 st.title("Image to Image Diffusion")
 
@@ -10,10 +13,13 @@ st.write("Select a base image")
 option = st.selectbox("Choose a number", ("1", "2", "3", "4"))
 
 
-image_file = "data/flower" + str(option) + ".jpg"
+image_name = "flower" + str(option) + ".jpg"
+image_file = DATAPATH / image_name
+
 st.write(image_file)
 
-st.image(image_file)
+image = Image.open(image_file)
+st.image(image)
 
 
 user_prompt = st.text_input(label="Add a prompt")
@@ -26,4 +32,5 @@ if st.button("Modify:"):
         data=json.dumps(inputs),
     )
 
-st.image("image.png")
+response_image_file = DATAPATH / "image.png"
+st.image(response_image_file)
