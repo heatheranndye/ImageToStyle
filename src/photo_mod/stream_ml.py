9b0ce8848b8d87f80244e43882409389eec77c83
heatheranndye <32517963+heatheranndye@users.noreply.github.com>
2023-01-11 15:08:38 -0600

    stupid addresses

--- src/photo_mod/stream_ml.py
+++ src/photo_mod/stream_ml.py
@@ -30,7 +30,7 @@ inputs = {"number_1": user_prompt1, "number_2": user_prompt2}
 
 if st.button("Modify:"):
     res = requests.post(
-        url="http://127.0.0.1:8000/add",
+        url="http://localhost:8000/add",
         data=json.dumps(inputs),
     )
     st.write(res.text)
