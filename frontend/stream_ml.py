aada1335ace9fd287426e07cf9df364c82c33f2d
heatheranndye <32517963+heatheranndye@users.noreply.github.com>
2023-02-06 12:53:42 -0600

    Docker setup changes

--- frontend/stream_ml.py
+++ frontend/stream_ml.py
@@ -1,27 +1,8 @@
 import streamlit as st
-import pathlib
-from PIL import Image
 import json
 import requests
 
-DATAPATH = pathlib.Path(__file__).parent / "data"
-
-st.title("Image to Image Diffusion")
-
-st.write("Select a base image")
-
-option = st.selectbox("Choose a number", ("1", "2", "3", "4"))
-
-
-image_name = f"flower{option}.jpg"
-image_file = DATAPATH / image_name
-
-# st.write(image_file)
-
-# image = Image.open(image_file)
-# st.image(image)
-
-
+st.title("Trial")
 user_prompt1 = st.number_input(label="value1")
 user_prompt2 = st.number_input(label="value2")
 
@@ -29,6 +10,7 @@ user_prompt2 = st.number_input(label="value2")
 inputs = {"number_1": user_prompt1, "number_2": user_prompt2}
 
 if st.button("Modify:"):
+
     res = requests.post(
         url="http://localhost:8000/add",
         data=json.dumps(inputs),
